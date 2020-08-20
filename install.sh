ef5e667c5729e838b7c3e7842b4bed104ec7c433
daoshuti <daoshuti@163.com>
2020-08-20 11:52:58 +0800

    update install.sh
    Signed-off-by: daoshuti <daoshuti@163.com>

--- install.sh
+++ install.sh
@@ -5,30 +5,40 @@
 # Description: install and reinstall dst_vimconfig
 
 
-echo Installing vim ...
+echo "Installing vim ..."
 sudo apt install vim
 
-echo Installing ctags cscope ...
+echo "Installing ctags cscope ..."
 sudo apt install ctags cscope
 
-echo Remove old vimcofnig ...
-echo Delete ~/.vim ...
-rm -rf ~/.vim
-echo Delete ~/.vimrc ...
-rm -rf ~/.vimrc
+read -N 1 -p "Do you want delete old config?(y/n):" answer
+echo ""
+if [ $answer == "Y" -o $answer == "y" ]; then
+	echo "Remove old vimcofnig ..."
+	echo "Delete ~/.vim ..."
+	rm -rf ~/.vim
+	echo "Delete ~/.vimrc ..."
+	rm -rf ~/.vimrc
+fi
 
-echo Link dst_vimconfig/imrc to ~/.vimrc ...
+if [ -d ~/.vim -o -f ~/.vimrc ]; then
+	echo "Install Failed!"
+	echo "Please backup your ~/.vimrc and ~/.vim"
+	exit 1
+fi
+
+echo "Link dst_vimconfig/imrc to ~/.vimrc ..."
 ln -s vimrc $HOME/.vimrc
 
-echo Installing vim-plug to ~/.vim/autoload/plug.vim ...
+echo "Installing vim-plug to ~/.vim/autoload/plug.vim ..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo Installing dst_vimconfig/create_cscope.sh to ~/.vim/shell/create_cscope.sh ...
+echo "Installing dst_vimconfig/create_cscope.sh to ~/.vim/shell/create_cscope.sh ..."
 mkdir -p ~/.vim/shell
 cp ./create_cscope.sh ~/.vim/shell
 
-echo Installing vim plugins ...
+echo "Installing vim plugins ..."
 vim +PlugInstall
 
-echo Done.
+echo "Done."
